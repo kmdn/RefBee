b5f3e177a3c0df51e4f69bdbdd227a887ab8dd57
Nicholas Popovic <nicpopovic@me.com>
2021-09-16 13:42:50 +0200

    dimensions paper titles for id

---
+++ dimensions.py
@@ -0,0 +1,12 @@
+import urllib.request
+import json
+
+def paper_titles_for_id(person_id):
+
+    url = f'https://app.dimensions.ai/discover/publication/results.json?and_facet_researcher=ur.{person_id}'
+    output = json.load(urllib.request.urlopen(url))
+    titles = [x['title'] for x in output['docs']]
+    return titles
+
+# print(paper_titles_for_id("010763216510.13"))
+
