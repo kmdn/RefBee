961b5377137937c5049b6c37ee71e58e5a106440
Nicholas Popovic <nicpopovic@me.com>
2021-09-16 13:32:20 +0200

    viaf paper titles for id

---
+++ viaf.py
@@ -0,0 +1,11 @@
+import urllib.request
+import json
+
+def paper_titles_for_id(person_id):
+
+    url = f'https://viaf.org/viaf/{person_id}/viaf.json'
+    output = json.load(urllib.request.urlopen(url))
+    titles = [x['title'] for x in output['titles']['work']]
+    return titles
+
+# print(paper_titles_for_id(4412150085882215060005))
