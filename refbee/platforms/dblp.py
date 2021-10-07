7122c9b67eca8cf785f6b140fe1b106484813fb0
Nicholas Popovic <nicpopovic@me.com>
2021-10-07 14:52:30 +0200

    changed xml decoding to fix problems with html tags in titles

--- refbee/platforms/dblp.py
+++ refbee/platforms/dblp.py
@@ -1,19 +1,24 @@
 import urllib.request
 from xml.etree.ElementTree import ElementTree
 
+import re
 from lxml import etree
 import pprint
 
 
 def retrieve_titles(input_xml=""):
+   # print(str(input_xml.read()))
     root = etree.XML(input_xml.read())
     #print(dir(root))
     # print(root.getchildren())
+    p = re.compile(r'<.*?>')
     titles = set()
     for element in root.iter():
+        #print("Element: %s %s" % (element.tag, element.text))
         if element.tag == "title":
-            titles.add(element.text)
-            #print("Element: %s %s " % (element.tag, element.text))
+            # titles.add(element.text)
+            titles.add(p.sub('', str(etree.tostring(element).decode("utf-8"))))
+            # print("Element: %s %s %s %s" % (element.tag, element.text, p.sub('', str(etree.tostring(element).decode("utf-8"))), [e.text for e in element.iter()]))
     return list(titles)
 
 
