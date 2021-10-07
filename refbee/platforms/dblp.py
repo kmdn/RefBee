cbf28c6ac2fad61d58470faad4900593d14c6d6a
Nicholas Popovic <nicpopovic@me.com>
2021-10-07 15:11:35 +0200

    a better fix, hopefully

--- refbee/platforms/dblp.py
+++ refbee/platforms/dblp.py
@@ -1,9 +1,5 @@
 import urllib.request
-from xml.etree.ElementTree import ElementTree
-
-import re
 from lxml import etree
-import pprint
 
 
 def retrieve_titles(input_xml=""):
@@ -11,14 +7,11 @@ def retrieve_titles(input_xml=""):
     root = etree.XML(input_xml.read())
     #print(dir(root))
     # print(root.getchildren())
-    p = re.compile(r'<.*?>')
     titles = set()
     for element in root.iter():
-        #print("Element: %s %s" % (element.tag, element.text))
+        #print("Element: %s %s" % (element.tag, ''.join(element.itertext())))
         if element.tag == "title":
-            # titles.add(element.text)
-            titles.add(p.sub('', str(etree.tostring(element).decode("utf-8"))))
-            # print("Element: %s %s %s %s" % (element.tag, element.text, p.sub('', str(etree.tostring(element).decode("utf-8"))), [e.text for e in element.iter()]))
+            titles.add(''.join(element.itertext()))
     return list(titles)
 
 
